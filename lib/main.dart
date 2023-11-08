7256f60dd58f9edeb5f235aa6b5901737222d82a
Hrithin1010 <hrithinak@gmail.com>
2023-11-08 08:26:14 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:calculator/calculator_main.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MainPage(),
+  ));
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: CalculatorMain(),
+    );
+  }
+}
